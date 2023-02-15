3f5f46249944b9434206540f55da8945fd8e2ce0
Omar Baldi <omar.baldi@maersk.com>
2023-02-15 21:41:17 +0100

    Added readme file to explain purpose of challenge + rendered initial starting point boxes into DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  const [boxes, setBoxes] = useState<string[]>([]);
+
+  useEffect(() => {
+    const length = 3;
+    const initialBoxes = [...Array(length).keys()].map(
+      (n, _) => `Box ${n.toString().padStart(2, '0')}`
+    );
+
+    setBoxes(initialBoxes);
+  }, []);
+
+  return (
+    <div className='App'>
+      <div className='boxesWrapper'>
+        {boxes.map((box, index: number) => {
+          return <div key={`bx-${box}-#${index}`} className='box' />;
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/README.md
@@ -0,0 +1,9 @@
+# Front-end challenge
+
+### Description
+
+The purpose of this challenge is to let the user insert a new box as soon as the user clicks in between 2 boxes. If instead a box is clicked directly, the user will be able to edit the content.
+
+### Consideration
+
+The user will be prompted with an odd number of boxes, in order to let him click in between 2 of them to insert a new one.
