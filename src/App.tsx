afa383aefc739616383f494d7ff4c4118f14410e
Omar Baldi <omar.baldi@maersk.com>
2023-02-16 16:27:36 +0100

    Implemented functionality to detect box click and update box content on input change

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   const boxesWrapperRef = useRef<HTMLDivElement>(null);
   const [boxes, setBoxes] = useState<string[]>([]);
+  const [boxIndexClicked, setBoxIndexClicked] = useState<number>();
 
   useEffect(() => {
     const length = 3;
@@ -27,14 +28,38 @@ function App() {
    * last biggest offset value in which the x value do not exceed.
    * That will correspond to the last element and the element before
    * will be the start.
+   *
+   * TODO: transform this into custom hook that takes the
+   * TODO: ...x and y axis values as well as the box ref..
+   * TODO: ...and return the updated elements to render into the DOM
+   *
    */
   useEffect(() => {
     const handleEventListenerClick = ({
       y: yAxisValue,
       x: xAxisValue,
+      target,
     }: MouseEvent): void => {
       const parentElement: HTMLDivElement | null = boxesWrapperRef.current;
       if (!parentElement || parentElement.children.length <= 0) return;
+
+      /**
+       * In order to detect if user has clicked inside one
+       * of the boxes I need to compare the current target
+       * with one of the boxes and if the same, I allow
+       * the user to edit the title.
+       */
+      if (target !== boxesWrapperRef.current) {
+        const boxIndexClicked = [...parentElement.children].findIndex(
+          (el) => el === target
+        );
+
+        setBoxIndexClicked((prevIndex) =>
+          boxIndexClicked >= 0 ? boxIndexClicked : prevIndex
+        );
+
+        return;
+      }
 
       const [firstBoxElement] = parentElement.children;
       const { offsetTop, clientHeight } = firstBoxElement as HTMLDivElement;
@@ -46,6 +71,8 @@ function App() {
        * In order to detect if the user has clicked in between 2 elements
        * I need to check the offsetLeft of each one of the boxes, to know
        * when to stop.
+       *
+       * TODO: transform this into function
        */
 
       const boxes = [...parentElement.children];
@@ -83,6 +110,20 @@ function App() {
     };
   }, []);
 
+  const handleBoxInputChange = (e: React.ChangeEvent): void => {
+    const { value: updatedBoxValue, dataset } = e.target as HTMLInputElement;
+    const boxIndex: string | undefined = dataset['boxIndex'];
+
+    if (typeof boxIndex === 'undefined' || isNaN(+boxIndex)) return;
+
+    setBoxes((prevBoxes) => {
+      const updatedBoxes = [...prevBoxes];
+      updatedBoxes[+boxIndex] = updatedBoxValue;
+
+      return updatedBoxes;
+    });
+  };
+
   return (
     <div className='App'>
       <div
@@ -93,7 +134,17 @@ function App() {
         {boxes.map((boxLabel, index: number) => {
           return (
             <div key={`bx-${boxLabel}-#${index}`} className='box'>
-              {boxLabel}
+              {boxIndexClicked === index ? (
+                <input
+                  type='text'
+                  autoFocus
+                  value={boxLabel}
+                  data-box-index={index}
+                  onChange={handleBoxInputChange}
+                />
+              ) : (
+                boxLabel
+              )}
             </div>
           );
         })}
